419fe5271fcadc55ad3501b98e0eafe83e97234a
sohail042414 <sohail042414@gmail.com>
2017-11-06 01:05:08 +0500

    initial push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ApplicationsComponent } from './applications/applications.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ApplicationsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
